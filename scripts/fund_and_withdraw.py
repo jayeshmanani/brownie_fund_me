08546224773384021254c997ef41155a9eba9c25
Jayesh Manani <jsmanani@gmail.com>
2023-12-27 08:54:07 +0100

    added all files

---
+++ scripts/fund_and_withdraw.py
@@ -0,0 +1,22 @@
+from brownie import FundMe
+from scripts.helpful import get_account
+
+def fund():
+    fund_me = FundMe[-1]
+    account = get_account()
+    entrance_fee = fund_me.getEntranceFee()
+    print(f"The current entry fee is {entrance_fee}")
+    print("Funding...")
+    fund_me.fund({"from": account, "value": entrance_fee})
+    print("Funded!")
+
+def withdraw():
+    fund_me = FundMe[-1]
+    account = get_account()
+    print("Withdrawing...")
+    fund_me.withdraw({"from": account})
+    print("Withdrawn!")
+
+def main():
+    fund()
+    withdraw()
